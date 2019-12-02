4f6f94477eb94b99131a15e0299d92f3b5ff9143
Francis Lalonde <fralalonde@C02TWE0JHTD5@bnc.ca>
2019-12-02 10:09:56 -0500

    Day 2 complete

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -2,42 +2,67 @@ use std::io;
 //use std::env;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
-use std::collections::HashMap;
 use anyhow::*;
+
+fn what(ops: &[usize], i: usize) -> usize {
+    let idx = ops[i];
+    ops[idx]
+}
+
+fn whoo(ops: &mut Vec<usize>, i: usize, set: usize){
+    let idx = ops[i];
+    ops[idx] = set
+}
+
+fn run(mut pos: Vec<usize>, a: usize, b: usize) -> Result<usize> {
+    let mut i = 0;
+
+    pos[1] = a;
+    pos[2] = b;
+    loop {
+        let op = pos[i];
+        match op {
+            1 => {
+                let a =  what(&pos, i + 1);
+                let b =  what(&pos, i + 2);
+                whoo(&mut pos, i+ 3,  a + b)
+            }
+            2 => {
+                let a =  what(&pos, i + 1);
+                let b =  what(&pos, i + 2);
+                whoo(&mut pos, i+ 3,  a * b)
+            }
+            99 => break,
+            _ => return Err(anyhow!("Mfuck")),
+        };
+        i += 4;
+    }
+    Ok(pos[0])
+}
 
 fn main() -> Result<()> {
     let f = File::open("02/input")?;
     let file = BufReader::new(&f);
-    let mut ops = file.lines().into_iter()
+    let mut og: Vec<usize> = file.lines().into_iter()
         .filter_map(|x| x.ok())
         .flat_map(|s| s.split(",")
             .map(|x| x.clone().parse::<usize>().unwrap())
-            .collect::<Vec<usize>>());
+            .collect::<Vec<usize>>())
+        .collect();
 
-    let mut positions = HashMap::new();
-    positions.insert(1, 12);
-    positions.insert(2, 2);
+    // part A
+    println!("A: {}", run(og.clone(), 12, 2)?);
 
-    loop {
-        match ops.next().unwrap() {
-            1 => {
-                let a: usize = *positions.get(&ops.next().with_context(|| "A")?).unwrap_or(&12);
-                let b: usize = *positions.get(&ops.next().with_context(|| "B")?).unwrap_or(&2);
-                let c: usize = ops.next().unwrap();
-                positions.insert(c, a + b);
+    // part B
+    'zzz:
+    for a in 0..99 {
+        for b in 0..99 {
+            if run(og.clone(), a, b)? == 19690720 {
+                println!("B {}", 100 * a + b);
+                break 'zzz;
             }
-            2 => {
-                let a: usize = *positions.get(&ops.next().with_context(|| "A")?).unwrap_or(&12);
-                let b: usize = *positions.get(&ops.next().with_context(|| "B")?).unwrap_or(&2);
-                let c: usize = ops.next().unwrap();
-                positions.insert(c, a * b);
-            }
-            99 => break,
-            _ => return Err(anyhow!("Mfuck")),
         }
     }
-
-    println!("0: {}", positions.get(&0).unwrap());
 
     Ok(())
 }
