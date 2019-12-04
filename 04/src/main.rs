ad49a3d03b532cf842682085fc4ca3d979aafce3
Francis Lalonde <fralalonde@gmail.com>
2019-12-04 11:49:01 -0500

    Day4 LOL

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -6,18 +6,18 @@ use std::ops::Range;
 use std::cmp::max;
 
 /// returns true if max has been reached
-fn lap(og: &[usize], max_v: usize, mut d: usize, pre_v: usize, pre_i: usize, seq: bool, comb: &mut usize) -> bool {
-    let start = max(pre_i, og[d - 1]);
-    let seq = seq || pre_i == start;
+fn lap(og: &[usize], max_v: usize, mut d: usize, pre_v: usize, pre_i: usize, pseq: bool, comb: &mut usize) -> bool {
+    let start = max(pre_i, og[d]);
     let pre_v = pre_v * 10;
     for i in start..10 {
         let z = pre_v + i;
-        if d == og.len() {
+        if z >= max_v { return true };
+        let seq = if d > 0 {pseq || pre_i == i} else {false};
+        if d == og.len() - 1 {
             if seq {
                 println!("{}", z);
                 *comb += 1
             }
-            if z >= max_v { return true };
         } else {
             if lap(og, max_v, d + 1, z, i, seq, comb) { return true }
         }
@@ -27,7 +27,7 @@ fn lap(og: &[usize], max_v: usize, mut d: usize, pre_v: usize, pre_i: usize, seq
 
 fn count(og: &[usize], max_v: usize) -> usize {
     let mut comb: usize = 0;
-    lap(og, max_v, 1, 0, 0, false, &mut comb);
+    lap(og, max_v, 0, 0, 0, false, &mut comb);
     comb
 }
 
@@ -48,5 +48,12 @@ mod test {
     fn chin() {
         let c = count(&[0, 0], 99);
         println!("comb {}", c);
+
+        let c = count(&[5, 3, 2], 874);
+        println!("comb {}", c);
+
+        let c = count(&[5, 3, 2], 421);
+        println!("comb {}", c);
+
     }
 }
