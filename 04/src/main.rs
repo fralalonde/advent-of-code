7933bf0afff01868f0d300e3bca7dc51a0ac58ae
Francis Lalonde <fralalonde@gmail.com>
2019-12-03 23:46:57 -0500

    Day4 await

---
+++ 04/src/main.rs
@@ -0,0 +1,19 @@
+use std::io;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+use anyhow::*;
+
+fn main() -> Result<()> {
+
+    println!("min dist");
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod test {
+
+    #[test]
+    fn bop() {
+    }
+}
